91f13410fe7bf311a0eb80b6b48c43db598d2eab
Alexander Weiss <weiss.alexander.96@gmail.com>
2020-05-20 15:55:24 +0200

    Finished yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+# ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Creating restaurants...'
+chezpauli = { name: 'ChezPauli', address: 'Grolmanstrasse 21', phone_number: '2637519', category: 'chinese'}
+savigny = { name: 'Savigny', address: 'Grolmanstrasse 54', phone_number: '827927', category: 'french'}
+madchenitaliener = { name: 'Madchenitaliener', address: 'Kantstarsse 192', phone_number: '261818720', category: 'italian'}
+bra = { name: 'Bra', address: 'Kuhdamm 211', phone_number: '739272', category: 'japanese'}
+tapas = { name: 'Tapas32', address: 'Hegendamm 321', phone_number: '389898', category: 'belgian'}
+
+[ chezpauli, savigny, madchenitaliener, bra, tapas ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
